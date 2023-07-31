857f3a6a3d13b797613e972641e78aa0bd39f6c2
Delziron Braz <44593154+DelzironBraz@users.noreply.github.com>
2023-07-31 18:31:09 -0300

    feat: frontend mentor - preview product (#1)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreviewProductComponent } from './_pages/components/preview-product/preview-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PreviewProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
