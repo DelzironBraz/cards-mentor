3340d09c0790d55314836f3ef5f3f8fed536672a
Delziron Braz <44593154+DelzironBraz@users.noreply.github.com>
2023-08-01 01:50:33 -0300

    feat: frontend mentor: QR code component (#4)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderSummaryComponent } from './_pages/components/order-summary/order-summary.component';
+import { QrCodeComponent } from './_pages/components/qr-code/qr-code.component';
 
 const routes: Routes = [
-  { path: '', component: OrderSummaryComponent }
+  { path: '', component: QrCodeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PreviewProductComponent } from './_pages/components/preview-product/preview-product.component';
 import { NftCardComponent } from './_pages/components/nft-card/nft-card.component';
 import { OrderSummaryComponent } from './_pages/components/order-summary/order-summary.component';
+import { QrCodeComponent } from './_pages/components/qr-code/qr-code.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PreviewProductComponent,
     NftCardComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    QrCodeComponent
   ],
   imports: [
     BrowserModule,
