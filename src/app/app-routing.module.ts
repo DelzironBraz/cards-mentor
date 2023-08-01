e1294d344caa04246e3a839746bbd2637ecd9028
Delziron Braz <delziron.braz@hotmail.com>
2023-08-01 00:39:10 -0300

    feat: frontend mentor: Order summary component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NftCardComponent } from './_pages/components/nft-card/nft-card.component';
+import { OrderSummaryComponent } from './_pages/components/order-summary/order-summary.component';
 
 const routes: Routes = [
-  { path: '', component: NftCardComponent }
+  { path: '', component: OrderSummaryComponent }
 ];
 
 @NgModule({
